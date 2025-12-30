cb56635a88e20fba026ac755adfc673be5343ecc
arkadiuszNowakDev <arkadiuszNowakDev@gmail.com>
2025-12-30 23:11:18 +0100

    creating global style

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from 'react'
 import App from '@src/App'
 import { createRoot } from 'react-dom/client'
 
+import '@src/common/styles/global.scss'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
